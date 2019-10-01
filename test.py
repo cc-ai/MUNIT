647c97e9ac6ba9712b573659a4817433f4f61e82
gcosne <cosne.gautier@gmail.com>
2019-10-01 14:10:20 -0400

    Update test.py for inference with G2
    Update test.py to infer through G2 with square random crop | good for now, see how to debug later

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ from torchvision import transforms
 from PIL import Image
 import tqdm as tq
 import glob
+from torchvision.transforms import functional as F
 
 # Parse arguments
 parser = argparse.ArgumentParser()
@@ -24,6 +25,7 @@ parser.add_argument("--output_folder", type=str, help="output image directory")
 parser.add_argument("--checkpoint", type=str, help="checkpoint of generator")
 parser.add_argument("--style", type=str, default="", help="style image path")
 parser.add_argument("--seed", type=int, default=10, help="random seed")
+parser.add_argument("--HD_checkpoint", type=str, default=".", help="HD_checkpoint")
 
 parser.add_argument(
     "--synchronized",
@@ -64,9 +66,26 @@ trainer = MUNIT_Trainer(config)
 # Load the model (here we currently only load the latest model architecture: one single style)
 try:
     state_dict = torch.load(opts.checkpoint)
-    trainer.gen.load_state_dict(state_dict["2"])
+    # overwrite entries in the existing state dict
+    gen_dict = trainer.gen.state_dict()
+
+    if trainer.gen_state == 1:
+        gen_dict.update(state_dict["2"]) 
+        trainer.gen.load_state_dict(gen_dict)
+        print('Loaded gen ckpt')
 except:
     sys.exit("Cannot load the checkpoints")
+    
+if opts.HD_checkpoint != ".":
+    try:
+        state_dict = torch.load(opts.HD_checkpoint)
+        trainer.gen.localUp.load_state_dict(state_dict["localUp"])
+        trainer.gen.localDown.load_state_dict(state_dict["localDown"]) 
+        print('Loaded HD ckpt')
+    except:
+        sys.exit("Cannot load the gen HD checkpoints")
+
+
 
 # Send the trainer to cuda
 trainer.cuda()
@@ -74,6 +93,7 @@ trainer.eval()
 
 # Set param new_size
 new_size = config["new_size"]
+new_size_HD = config["new_size_HD"]
 
 # Define the list of non-flooded images
 list_non_flooded = glob.glob(opts.input+'*')
@@ -86,39 +106,86 @@ if len(list_non_flooded) ==0:
 with torch.no_grad():
     # Define the transform to infer with the generator
     transform = transforms.Compose(
-        [
-            transforms.Resize(new_size),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ]
-    )
+            [
+                transforms.Resize(new_size),
+                transforms.RandomCrop((new_size,new_size)),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
     # Load and Transform the Style Image
     style_image = (
         Variable(transform(Image.open(opts.style).convert("RGB")).unsqueeze(0).cuda())
     )
     # Extract the style from the Style Image
     _, s_b = trainer.gen.encode(style_image, 2)
-    
-    for j in tq.tqdm(range(len(list_non_flooded))):
+
+    for jjj in tq.tqdm(range(len(list_non_flooded))):
         
         # Define image path
-        path_xa = list_non_flooded[j]
+        path_xa = list_non_flooded[jjj]
         
-        # Load and transform the non_flooded image
-        x_a = Variable(
-            transform(Image.open(path_xa).convert("RGB")).unsqueeze(0).cuda()
+        #####################################################
+        image = Image.open(path_xa).convert("RGB")
+        # Resizing to the closest multiple of 4 (pix2pix)
+        new_size_hd =512
+        ow, oh = image.size
+        aspect_ratio = ow/oh
+        if aspect_ratio <1:
+            ow, oh = new_size_hd,int(round(new_size_hd*oh/ow))
+        else:
+            ow, oh = int(round(new_size_hd*ow/oh)),new_size_hd
+            
+        base = 4
+        h = int(round(oh / base) * base)
+        w = int(round(ow / base) * base)
+        # print('debugging mask transform 2 size',mask.size)
+        # Define Resize HD (multiple of 4) used by the downsampler
+        resize_HD = transforms.Resize((h,w))
+        # Resize for HD
+        image_HD = resize_HD(image)
+        
+        i, j, h, w = transforms.RandomCrop.get_params(
+            image_HD, output_size=(new_size_hd, new_size_hd)
         )
+        image_HD = F.crop(image_HD, i, j, h, w)
+        
+        # Define Resize for G1
+        resize    = transforms.Resize((new_size,new_size))
+        
+        # Resize for G1
+        image = resize(image_HD)
+       
+        to_tensor = transforms.ToTensor()
+        image_HD  = to_tensor(image_HD)
+        image     = to_tensor(image)
+
+        # print('debugging mask transform 5 size',mask.size)
+        # Normalize
+        normalizer = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        x_a_HD = normalizer(image_HD)
+        x_a    = normalizer(image)
+        
+        x_a = x_a.unsqueeze(0).cuda()
+        x_a_HD = x_a_HD.unsqueeze(0).cuda()
+        #####################################################
+        
+        # Downsample
+        Downsampled_x_a = trainer.gen.localDown(x_a_HD)
+        
         # Extract content and style
         c_a, _ = trainer.gen.encode(x_a, 1)
 
         # Perform cross domain translation
-        x_ab = trainer.gen.decode(c_a, s_b, 2)
-
+        x_ab, embedding_x_ab = trainer.gen.decode(c_a, s_b, 2, return_content=True)
+        
+        x_ab_HD = trainer.gen.localUp(embedding_x_ab + Downsampled_x_a)
+        
         # Denormalize .Normalize(0.5,0.5,0.5)...
-        outputs = (x_ab + 1) / 2.0
+        outputs = (x_ab_HD + 1) / 2.0
 
         # Define output path
-        path = os.path.join(opts.output_folder, "output{:03d}.jpg".format(j))
+        path = os.path.join(opts.output_folder, "output{:03d}.jpg".format(jjj))
 
         # Save image 
         vutils.save_image(outputs.data, path, padding=0, normalize=True)
