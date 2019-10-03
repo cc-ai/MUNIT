7e9d059ab84a82e69929823df4c9bf5efceca78b
gcosne <cosne.gautier@gmail.com>
2019-10-03 15:11:32 -0400

    Modify the L2 loss on synthetic pairs
    We want to compute the L2 loss between an non-flooded image and the fake flooded image only on the pixels of the pairs that are exactly the same

--- trainer.py
+++ trainer.py
@@ -288,8 +288,16 @@ class MUNIT_Trainer(nn.Module):
         self.loss_gen_recon_c_b = self.recon_criterion(c_b_recon, c_b)
         
         # Synthetic reconstruction loss
-        self.loss_gen_recon_synth = self.recon_criterion_mask(x_ab, x_b,mask_b) +\
-                                    self.recon_criterion_mask(x_ba, x_a,mask_a)  if synth else 0
+        if synth:
+            print('mask_b.shape', mask_b.shape)
+            # Define the mask of exact same pixel among a pair
+            mask_alignment = (torch.sum(torch.abs(x_a - x_b), 1) == 0).unsqueeze(0)
+            mask_alignment = mask_alignment.type(torch.cuda.FloatTensor)
+            print('mask_alignment.shape', mask_alignment.shape)
+            
+        
+        self.loss_gen_recon_synth = self.recon_criterion_mask(x_ab, x_b, 1-mask_alignment) + \
+                                    self.recon_criterion_mask(x_ba, x_a, 1-mask_alignment)  if synth else 0
         
         if self.recon_mask:
             self.loss_gen_cycrecon_x_a = (
