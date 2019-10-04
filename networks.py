79cbced8ae1db62603f115545d143ff9ec86f9f0
gcosne <cosne.gautier@gmail.com>
2019-10-04 11:14:39 -0400

    First commit extending MUNIT discriminator
    Next step is to map the ckpt dictionnary to resume MUNIT discriminator in the HD discriminator at the correct scale

--- networks.py
+++ networks.py
@@ -110,6 +110,100 @@ class MsImageDis(nn.Module):
         return loss
 
 
+class MsImageDisExtended(nn.Module):
+    # Multi-scale discriminator architecture
+    def __init__(self, input_dim, params):
+        super(MsImageDisExtended, self).__init__()
+        self.n_layer = params["n_layer"]
+        self.gan_type = params["gan_type"]
+        self.dim = params["dim"]
+        self.norm = params["norm"]
+        self.activ = params["activ"]
+        self.num_scales = params["num_scales"]+1
+        self.pad_type = params["pad_type"]
+        self.input_dim = input_dim
+        self.downsample = nn.AvgPool2d(
+            3, stride=2, padding=[1, 1], count_include_pad=False
+        )
+        self.cnns = nn.ModuleList()
+        for _ in range(self.num_scales):
+            self.cnns.append(self._make_net())
+
+    def _make_net(self):
+        dim = self.dim
+        cnn_x = []
+        cnn_x += [
+            Conv2dBlock(
+                self.input_dim,
+                dim,
+                4,
+                2,
+                1,
+                norm="none",
+                activation=self.activ,
+                pad_type=self.pad_type,
+            )
+        ]
+        for i in range(self.n_layer - 1):
+            cnn_x += [
+                Conv2dBlock(
+                    dim,
+                    dim * 2,
+                    4,
+                    2,
+                    1,
+                    norm=self.norm,
+                    activation=self.activ,
+                    pad_type=self.pad_type,
+                )
+            ]
+            dim *= 2
+        cnn_x += [nn.Conv2d(dim, 1, 1, 1, 0)]
+        cnn_x = nn.Sequential(*cnn_x)
+        return cnn_x
+
+    def forward(self, x):
+        outputs = []
+        for model in self.cnns:
+            outputs.append(model(x))
+            x = self.downsample(x)
+        return outputs
+
+    def calc_dis_loss(self, input_fake, input_real):
+        # calculate the loss to train D
+        outs0 = self.forward(input_fake)
+        outs1 = self.forward(input_real)
+        loss = 0
+
+        for it, (out0, out1) in enumerate(zip(outs0, outs1)):
+            if self.gan_type == "lsgan":
+                loss += torch.mean((out0 - 0) ** 2) + torch.mean((out1 - 1) ** 2)
+            elif self.gan_type == "nsgan":
+                all0 = Variable(torch.zeros_like(out0.data).cuda(), requires_grad=False)
+                all1 = Variable(torch.ones_like(out1.data).cuda(), requires_grad=False)
+                loss += torch.mean(
+                    F.binary_cross_entropy(F.sigmoid(out0), all0)
+                    + F.binary_cross_entropy(F.sigmoid(out1), all1)
+                )
+            else:
+                assert 0, "Unsupported GAN type: {}".format(self.gan_type)
+        return loss
+
+    def calc_gen_loss(self, input_fake):
+        # calculate the loss to train G
+        outs0 = self.forward(input_fake)
+        loss = 0
+        for it, (out0) in enumerate(outs0):
+            if self.gan_type == "lsgan":
+                loss += torch.mean((out0 - 1) ** 2)  # LSGAN
+            elif self.gan_type == "nsgan":
+                all1 = Variable(torch.ones_like(out0.data).cuda(), requires_grad=False)
+                loss += torch.mean(F.binary_cross_entropy(F.sigmoid(out0), all1))
+            else:
+                assert 0, "Unsupported GAN type: {}".format(self.gan_type)
+        return loss    
+    
+    
 ##################################################################################
 # Generator
 ##################################################################################
--- trainer.py
+++ trainer.py
@@ -3,7 +3,7 @@ Copyright (C) 2017 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
 from comet_ml import Experiment
-from networks import AdaINGen, AdaINGen_double, MsImageDis, VAEGen,LocalUpsampler,AdaINGen_double_HD
+from networks import AdaINGen, AdaINGen_double, MsImageDis, VAEGen,LocalUpsampler,AdaINGen_double_HD,MsImageDisExtended
 from utils import (
     weights_init,
     get_model_list,
@@ -60,7 +60,11 @@ class MUNIT_Trainer(nn.Module):
         self.dis_b = MsImageDis(
             hyperparameters["input_dim_b"], hyperparameters["dis"]
         )  # discriminator for domain b
-         
+        self.dis_a_global = MsImageDisExtended(
+            hyperparameters["input_dim_a"], hyperparameters["dis"]
+        ) # Discriminator with one more scale 
+        
+        
         self.instancenorm = nn.InstanceNorm2d(512, affine=False)
         self.style_dim = hyperparameters["gen"]["style_dim"]
 
