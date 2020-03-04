2fda7e5d02aa05cbd9ed557ae558480da230577c
Sunand Raghupathi <raghupas@server.mila.quebec>
2020-03-04 11:03:40 -0500

    Fixing default output dir

--- scripts/train.py
+++ scripts/train.py
@@ -40,7 +40,7 @@ parser.add_argument(
     "--config", type=str, default="configs/config256.yaml", help="Path to the config file.",
 )
 parser.add_argument(
-    "--output_path", type=str, default="/network/tmp1/ccai/checkpoints/sun/", help="outputs path"
+    "--output_path", type=str, default=".", help="outputs path"
 )
 parser.add_argument("--resume", action="store_true")
 parser.add_argument("--trainer", type=str, default="MUNIT", help="MUNIT|UNIT")
